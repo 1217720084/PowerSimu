c4ed1ac8526e0a28f52b71ab6f10d2caac288660
paperplane123 <1697620694@qq.com>
2017-04-19 23:05:36 +0800

    没有索引的gycall部分方程

--- routine/NewTonpf.py
+++ routine/NewTonpf.py
@@ -6,7 +6,6 @@ def calcInc():
      Gycall()
      y=np.linalg.solve(system.DAE.Gy,system.DAE.g)   #直接调用linalg中的solve求解修正方程
      return -y
-
 def powerflow():
     """main power flow routine"""
     # general settings
@@ -15,10 +14,10 @@ def powerflow():
     iter_max = system.Settings.pf_max_iter
     convergence = True            #收敛
     tol = system.Settings.tol
-    system.Settings.error = tol + 1
+
 
     # main loop
-    while system.Settings.error > tol and iteration <= iter_max:
+    while max(abs(dae.g)) > tol and iteration <= iter_max:
          inc = calcInc()
          system.DAE.y += inc
          iteration += 1
@@ -27,3 +26,30 @@ def powerflow():
     if iteration > iter_max:
         print ('Reached maximum number of iterations')
         convergence = False
+    else:
+        #先求PV节点无功
+        # V[i] = system.PV.__dict__['_data']['V0'][i - 1]
+        # V[j] = system.PV.__dict__['_data']['V0'][j - 1]
+        # G[i][j] =
+        # B[i][j] =
+        # Va[i] =
+        # Va[j] =      #对应pv节点的电压相角
+        # P[i]
+        # Q[i]        #对应pv节点的P,Q
+        sumq[i]=0
+        for i in range(pv.self.n):
+            for j in range(bus.self.n):
+                if i~j:   #j与i直接相连
+                    sumq[i] = V[j] * (G[i][j] *sin(Va[i]-Va[j]) - B[i][j]* cos(Va[i]-Va[j]))
+            Q[i]=V[i]*sumq[i]
+
+        #求平衡节点有无功
+        sumps[bus.self.n] = 0
+        sumqs[bus.self.n] = 0
+        for j in range(bus.self.n):
+            if i~j:       #j与i直接相连
+                sumps[bus.self.n] = V[j] * (G[bus.self.n][j] * cos(Va[bus.self.n] - Va[j]) + B[bus.self.n][j] * sin(Va[bus.self.n] - Va[j]))
+                sumqs[bus.self.n] = V[j] * (G[bus.self.n][j] *  sin(Va[bus.self.n] - Va[j]) - B[bus.self.n][j] * cos(Va[bus.self.n] - Va[j]))
+        Ps[bus.self.n] = V[bus.self.n] * sumps[bus.self.n]
+        Qs[bus.self.n] = V[bus.self.n] * sumqs[bus.self.n]
+        print('result')
